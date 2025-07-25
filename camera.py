fixed incorrect pivot calculation for foldGrab

camera.py
@@ -1,3 +1,4 @@
+import numpy as np
 import math
 import pyrr
 
@@ -14,8 +15,7 @@ class Camera:
         y = math.sin(self.pitch)
         x = math.cos(self.yaw) * math.cos(self.pitch)
         z = math.sin(self.yaw) * math.cos(self.pitch)
-        self.facing = (x, y, z)
-        
+        self.facing = (x, y, z)        
 
     def orbit(self, yaw, pitch):
         self.yaw += yaw
@@ -46,5 +46,5 @@ class Camera:
         target = pyrr.vector3.create(x=0.0, y=0.0, z=0.0)
         up = pyrr.vector3.create(x=0.0, y=1.0, z=0.0)
 
-        self.view_transform = pyrr.matrix44.create_look_at(eye, target, up, dtype=None)
+        self.view_transform = pyrr.matrix44.create_look_at(eye, target, up, dtype=np.float32)
         glUniformMatrix4fv(modelViewLocation, 1, GL_FALSE, self.view_transform)

foldingengine.py
@@ -1,11 +1,13 @@
 import numpy as np
 import math
 import pyrr
+from randomoperations import *
 
 class Fold:
-    def __init__(self, faces, points):
+    def __init__(self, faces, points, projection_transform):
         self.faces = faces
         self.points = points
+        self.projection_transform = projection_transform
 
     def fold(self, point1, point2, face, angle):
         p1x, p1y, p1z = self.points[point1]
@@ -27,51 +29,53 @@ class Fold:
             dtype=np.float32
         )
 
-        self.faces[face].updateModelMatrix(pyrr.matrix44.multiply(fromOrigin, pyrr.matrix44.multiply(rotationMatrix, toOrigin)))
+        self.faces[face].updateModelMatrix(pyrr.matrix44.multiply(pyrr.matrix44.multiply(toOrigin, rotationMatrix), fromOrigin))
 
     def foldGrab(self, point1, point2, face, mouseinfo, view_transform):
         # Transform mouse data from pygame screen coordinates to OpenGl screen coordinates
         finalMousePos, mouseDisplacement, screenInfo = mouseinfo
-        finalMousePos = (
-            2*(finalMousePos[0] / screenInfo[0]) - 1,
-            2*(finalMousePos[1] / screenInfo[1]) - 1
+        initialMousePos = (
+            finalMousePos[0] - mouseDisplacement[0],
+            finalMousePos[1] - mouseDisplacement[1]
         )
-        mouseDisplacement = (
-            2*mouseDisplacement[0] / screenInfo[0],
-            2*mouseDisplacement[1] / screenInfo[1]
-        )
-
+        finalMousePos = convertToOpenGLCoordinates(finalMousePos[0], finalMousePos[1], screenInfo[0], screenInfo[1])
+        initialMousePos = convertToOpenGLCoordinates(initialMousePos[0], initialMousePos[1], screenInfo[0], screenInfo[1])
         # Create transformation matrix using model and view matrices
         model_transform = self.faces[face].model_transform
         transformMatrix = pyrr.matrix44.multiply(
-            view_transform,
-            model_transform
+            pyrr.matrix44.multiply(
+                model_transform,
+                view_transform
+            ),
+            self.projection_transform
         )
 
         # Transform crease points to screen coordinates
         point1Transformed = pyrr.matrix44.multiply(
-            transformMatrix,
-            pyrr.Vector4(self.points[point1] + (1,))
+            pyrr.Vector4(np.append(self.points[point1], 1.0)),
+            transformMatrix
         )
         point2Transformed = pyrr.matrix44.multiply(
-            transformMatrix,
-            pyrr.Vector4(self.points[point2] + (1,))
+            pyrr.Vector4(np.append(self.points[point2], 1.0)),
+            transformMatrix
         )
+
+        ndcP1 = point1Transformed / point1Transformed[3]
+        ndcP2 = point2Transformed / point2Transformed[3]
 
         # Define point for cursor to rotate around
         pivot = (
-            (point1Transformed[0]+point2Transformed[0])/2,
-            (point1Transformed[1]+point2Transformed[1])/2
-            )
+            (ndcP1[0]+ndcP2[0])/2,
+            (ndcP1[1]+ndcP2[1])/2
+        )
         
-        # Change mouse coordinates from pygame to OpenGL
         initialCoordinate = (
-            finalMousePos[0] - mouseDisplacement[0] - pivot[0],
-            -(finalMousePos[1] - mouseDisplacement[1] - pivot[1])
+            initialMousePos[0] - pivot[0],
+            initialMousePos[1] - pivot[1]
         )
         finalCoordinate = (
             finalMousePos[0] - pivot[0],
-            -(finalMousePos[1] - pivot[1])
+            finalMousePos[1] - pivot[1]
         )
 
         # Calculate change in angle
@@ -79,7 +83,7 @@ class Fold:
         theta1 = math.atan2(finalCoordinate[1], finalCoordinate[0])
 
         theta = theta1 - theta0
-        if point1Transformed[2] > point2Transformed[2]:
+        if point1Transformed[2] < point2Transformed[2]:
             theta = 0-theta
 
         self.fold(point1, point2, face, theta)